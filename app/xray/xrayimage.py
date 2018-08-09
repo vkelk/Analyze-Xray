43e353866e058d8d4c521bebb778be301cc8fba6
Vasko Kelkocev <pisimina@gmail.com>
2018-08-09 13:35:32 +0200

    imports outside of function

--- app/xray/xrayimage.py
+++ app/xray/xrayimage.py
@@ -1,14 +1,16 @@
+import os
+import numpy as np
+from keras.models import model_from_json
+from keras.preprocessing import image
+
 from app.config import MODEL_DIR
+
+json_file = open(os.path.join(MODEL_DIR, 'model.json'), 'r')
+loaded_model_json = json_file.read()
+json_file.close()
 
 
 def analyze_image(image_path):
-    import os
-    import numpy as np
-    from keras.models import model_from_json
-    from keras.preprocessing import image
-    json_file = open(os.path.join(MODEL_DIR, 'model.json'), 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     loaded_model.load_weights(os.path.join(
         MODEL_DIR,
